93eb879547d2bfed67e7242775f64f4639af48d9
TaccaT17 <tom@crammond.name>
2021-01-30 12:45:37 -0800

    Created and tested LightObject Component

---
+++ ColoredLight/Assets/Sandbox/Tom/LightObject.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightObject : MonoBehaviour
+{   
+    //TODO: put this function Gamemanager Singleton
+    public static Component GetOrCreateComponent<T>(out T component, GameObject gO) where T : Component
+    {
+        if (gO.GetComponent<T>() == null)
+        {
+            component = gO.gameObject.AddComponent<T>();
+        }
+        else
+        {
+            component = gO.GetComponent<T>();
+        }
+
+        return component;
+    }
+
+    public enum ColorOfLight
+    {
+        red,
+        yellow,
+        blue
+    }
+
+    public bool redBoolLight = false;
+
+    #region VARIABLES
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public int redLitsNeeded, yellowLitsNeeded, blueLitsNeeded;
+
+    private Dictionary<ColorOfLight, Light> litBy;
+    private Light initRedLight, initYellowLight, initBlueLight;
+
+    private MeshRenderer meshRendererRef;
+    private Collider colliderRef;
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #endregion
+
+
+
+    #region FUNCTIONS
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public void Start()
+    {
+        Init();
+    }
+
+    public void Update()
+    {
+        if (redBoolLight && litBy[ColorOfLight.red].currentLits < 1)
+        {
+            print("it's lit");
+
+            LightTouched(ColorOfLight.red);
+        }
+    }
+
+    public void Init()
+    {
+        GetOrCreateComponent(out colliderRef, this.gameObject);
+        GetOrCreateComponent(out meshRendererRef, this.gameObject);
+
+        Constructor(redLitsNeeded, yellowLitsNeeded, blueLitsNeeded);
+
+        CheckAndChangeOpacity();
+    }
+    /// <summary>
+    /// Parameters are how many of each color light needed to be lighting that object in order to "solidify" the object
+    /// </summary>
+    /// <param name="redLits"></param>
+    /// <param name="yellowLits"></param>
+    /// <param name="blueLits"></param>
+    public void Constructor(int redLits, int yellowLits, int blueLits)
+    {
+        initRedLight.neededLits = redLits;
+        initRedLight.currentLits = 0;
+        initYellowLight.neededLits = yellowLits;
+        initYellowLight.currentLits = 0;
+        initBlueLight.neededLits = blueLits;
+        initBlueLight.currentLits = 0;
+
+        //create Dictionaries
+        litBy = new Dictionary<ColorOfLight, Light>();
+        litBy.Add(ColorOfLight.red, initRedLight);
+        litBy.Add(ColorOfLight.yellow, initYellowLight);
+        litBy.Add(ColorOfLight.blue, initBlueLight);
+    }
+    
+    public int GetLitAmount(ColorOfLight lightColor)
+    {
+        return litBy[lightColor].currentLits;
+    }
+    public void LightTouched(ColorOfLight lightColor)
+    {
+        Light tempLight = litBy[lightColor];
+        tempLight.currentLits++;
+        litBy[lightColor] = tempLight;
+
+        CheckAndChangeOpacity();
+    }
+    public void LightLeft(ColorOfLight lightColor)
+    {
+        Light tempLight = litBy[lightColor];
+        tempLight.currentLits--;
+        litBy[lightColor] = tempLight;
+
+        CheckAndChangeOpacity();
+    }
+    private void CheckAndChangeOpacity()
+    {
+        int currentLitness = 0;
+        int neededLitNess = 0;
+
+        //get total lights lighting
+        foreach (KeyValuePair<ColorOfLight, Light> item in litBy)
+        {
+            //if needed amount is 0 don't count it
+            if (!(item.Value.neededLits.Equals(0)))
+            {
+                currentLitness += item.Value.currentLits;
+                neededLitNess += item.Value.neededLits;
+            }
+        }
+
+        //don't divide by 0
+        if (neededLitNess.Equals(0))
+        {
+            print("Oops. Trying to divide by 0. You forgot to set a LightObject's LitsNeeded");
+            return;
+        }
+
+        float opacityPercentage = (float)currentLitness / (float)neededLitNess;
+
+        print($"{gameObject.name} has currentLitness {currentLitness}");
+        print($"{gameObject.name} has neededLitNess {neededLitNess}");
+        print($"{gameObject.name} has opacity Percentage {opacityPercentage}");
+
+        //set new opacity
+        Color color = meshRendererRef.material.color;
+        color.a = opacityPercentage;
+        meshRendererRef.material.color = color;
+
+        if (opacityPercentage >= 1)
+        {
+            colliderRef.enabled = true;
+        }
+        else
+        {
+            colliderRef.enabled = false;
+        }
+
+    }
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #endregion
+
+    //STRUCT
+    private struct Light
+    {
+        public int currentLits;
+        public int neededLits;
+    }
+
+    
+}
