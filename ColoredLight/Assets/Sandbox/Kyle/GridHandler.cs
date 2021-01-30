531299add8c2071167f0f07de25bfe331ab215dc
Kahl21 <kahlpartlow@gmail.com>
2021-01-29 22:17:23 -0800

    -pushing for bryce
    -just scripts and resources folder

---
+++ ColoredLight/Assets/Sandbox/Kyle/GridHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class GridHandler 
+{
+    static char[,] _levelGrid;
+    static Object[,] _objectsGrid;
+    //static Player _playerRef;
+
+    static WorldHandler _worldRef;
+    public static void Init()
+    {
+        _levelGrid = new char[0, 0];
+        _objectsGrid = new Object[0, 0];
+        _objectsGrid[0,0] = null;
+    }
+
+    public static void LoadLevel(string Level)
+    {
+        
+    }
+
+    /*public static MovePlayer(eDirections dir)
+    {
+
+    }*/
+
+    public static void UnloadLevel()
+    {
+        _levelGrid = new char[0, 0];
+    }
+
+    public static WorldHandler SetWorldRef { set { _worldRef = value; } }
+}
