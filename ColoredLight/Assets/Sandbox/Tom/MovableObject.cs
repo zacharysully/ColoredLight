15b6a411620b3a84a3583e22eb74b00ff71a6687
TaccaT17 <tom@crammond.name>
2021-01-30 14:52:31 -0800

    Made MovableObject script

---
+++ ColoredLight/Assets/Sandbox/Tom/MovableObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    [SerializeField]
+    private float travelDistance = 1f;
+
+    float second = 0;
+
+    public enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    public void Move(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+                this.gameObject.transform.position += new Vector3(travelDistance, 0, 0);
+                break;
+            case Direction.South:
+                this.gameObject.transform.position += new Vector3(-travelDistance, 0, 0);
+                break;
+            case Direction.East:
+                this.gameObject.transform.position += new Vector3(0, 0, -travelDistance);
+                break;
+            case Direction.West:
+                this.gameObject.transform.position += new Vector3(0, 0, travelDistance);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
